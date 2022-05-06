0515e54c4cb2e6e617f9f8083068f4b11f16886d
LeandroNeto <leandrohock@gmail.com>
2022-05-06 08:07:08 -0300

    Criando a página Posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>06 de maio de 2022</time>
+            <strong>Título do Post</strong>
+            <p>Breve parágrafo sobre o post</p>
+          </a>
+          <a href="#">
+            <time>06 de maio de 2022</time>
+            <strong>Título do Post</strong>
+            <p>Breve parágrafo sobre o post</p>
+          </a>
+          <a href="#">
+            <time>06 de maio de 2022</time>
+            <strong>Título do Post</strong>
+            <p>Breve parágrafo sobre o post</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
